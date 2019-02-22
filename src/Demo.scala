244a38c691468f57700019e358982b76372b2c64
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2019-02-22 23:29:56 +0530

    Initial Commit

---
+++ src/Demo.scala
@@ -0,0 +1,15 @@
+//This is a initial versioned .scala calss
+//Below is an example for sumUp of two numbers
+
+object Demo {
+  def main(args: Array[String]) {
+
+    // Move to a new location
+    println("Hi")
+    println( sumUp2(10, 10))
+  }
+
+  def sumUp2(a: Int, b: Int): Int = {
+    a + b
+  }
+}
